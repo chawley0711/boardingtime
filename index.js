c457bffa6cd73a03274dd7ffdaecf88862c7536a
Unknown <CHawley@student.neumont.edu>
2018-02-27 21:41:54 -0700

    users can login

--- index.js
+++ index.js
@@ -49,9 +49,17 @@ app.post('/registerComplete', urlencodedParser, function(req, res){
     avatarString: req.cookies.avatarString
   }
   route.createUser(user);
-  res.render('registerComplete',
-  {
-    user: user
+  res.redirect('/');
+});
+app.post('/login', urlencodedParser, function(req, res){
+  User.find({username: req.body.username}, function(err, document){
+    if(document.password == req.body.password){
+      req.session.user = {
+        isAuthenticated: true,
+        username: req.body.username
+      };
+      res.redirect('/');
+    }
   });
 });
 app.get('/login', function(req, res){
@@ -80,18 +88,127 @@ app.get('/logout', function(req, res){
   });
 });
 
-app.post('/',urlencodedParser, function(req, res){
-  console.log(req.body.username);
-  if(req.body.username=='user' && req.body.pass=='password'){
-    req.session.user={
-      isAuthenticated: true,
-      username: req.body.username
-    };
-    res.redirect('/private');
-  }else{
-    res.redirect('/');
-  }
+// app.post('/',urlencodedParser, function(req, res){
+//   console.log(req.body.username);
+//   if(req.body.username=='user' && req.body.pass=='password'){
+//     req.session.user={
+//       isAuthenticated: true,
+//       username: req.body.username
+//     };
+//     res.redirect('/private');
+//   }else{
+//     res.redirect('/');
+//   }
   
+// });
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/data');
+
+var mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
+mdb.once('open', function (callback) {
+    // var admin = mdb.admin();
+    // admin.addUser('admin', 'adminpassword', function(err, result){
+    //     admin.authenticate('admin', 'adminpassword')
+    // })
 });
+
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    email: String,
+    age: String,
+    avatarString: String,
+    role: String
+});
+var messageSchema = mongoose.Schema({
+    username: String,
+    date: String,
+    contents: String
+});
+
+var User = mongoose.model('User_Collection', userSchema);
+var Messages = mongoose.model('Message_Collection', messageSchema);
+
+
+app.get('/peopleList', function (req, res) {
+    User.find(function (err, user) {
+        if (err) return console.error(err);
+        res.render('index', {
+            title: 'People List',
+            user: user
+        });
+    });
+});
+
+app.get('/create', function (req, res) {
+    res.render('create', {
+        title: 'Add User'
+    });
+});
+
+app.get('/createUser', function(user) {
+    console.log('asd');
+    var person = new User({
+        username: user.username,
+        password: user.password,
+        email: user.email,
+        age: user.age,
+        avatarString: user.avatarString,
+        role: "user"
+    });
+    console.log(person);
+    person.save(function (err, person) {
+        if (err) return console.error(err);
+        console.log(person.username + ' added');
+    });
+});
+
+app.get('/edit', function (req, res) {
+    User.findByUsername(req.params.id, function (err, user) {
+        if (err) return console.error(err);
+        res.render('edit', {
+            title: 'Edit Person',
+            user: user
+        });
+    });
+});
+
+app.get('/editUser', function (req, res) {
+    Person.findById(req.params.id, function (err, user) {
+        if (err) return console.error(err);
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.age = req.body.age;
+        user.email = req.body.email;
+        user.avatarString = req.cookie.avatarString;
+
+        User.save(function (err, person) {
+            if (err) return console.error(err);
+            console.log(req.body.name + ' updated');
+        });
+    });
+    res.redirect('/');
+
+});
+
+app.get('/delete', function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if (err) return console.error(err);
+        res.redirect('/');
+    });
+});
+
+app.get('/details', function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) return console.error(err);
+        res.render('details', {
+            title: user.username + "'s Details",
+            user: user
+        });
+    });
+});
+
 
 app.listen(3000);
